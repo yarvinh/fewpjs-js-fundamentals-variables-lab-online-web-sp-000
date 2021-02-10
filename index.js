8deae5b903fa64055b7ca7233faba56f64d707fe
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2021-02-10 03:07:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-const  companyName = 'Scuber'
-let  mostProfitableNeighborhood = 'Chelsea'
+const  companyName = 'Scuber';
+let  mostProfitableNeighborhood = 'Chelsea';
 
-let companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith';
